d3093a710259c9e4cd271d5b520ccafa7d1fc469
Marco Petutschnig <mpetutschnig@gmail.com>
2026-04-30 19:40:48 +0200

    update podman

--- pwa/src/pages/DocumentScanPage.tsx
+++ pwa/src/pages/DocumentScanPage.tsx
@@ -90,46 +90,6 @@ export default function DocumentScanPage() {
               bmp.close()
             }, 'image/jpeg', 0.8)
           }).catch(reject)
-        } else {
-          const objectUrl = URL.createObjectURL(f)
-          const img = new Image()
-
-          img.onload = () => {
-            const canvas = document.createElement('canvas')
-            const MAX_WIDTH = 1200
-            const MAX_HEIGHT = 1200
-            let width = img.width
-            let height = img.height
-
-            if (width > height) {
-              if (width > MAX_WIDTH) {
-                height *= MAX_WIDTH / width
-                width = MAX_WIDTH
-              }
-            } else {
-              if (height > MAX_HEIGHT) {
-                width *= MAX_HEIGHT / height
-                height = MAX_HEIGHT
-              }
-            }
-
-            canvas.width = width
-            canvas.height = height
-            const ctx = canvas.getContext('2d')
-            ctx?.drawImage(img, 0, 0, width, height)
-
-            canvas.toBlob((blob) => {
-              if (blob) {
-                const resizedFile = new File([blob], f.name, { type: 'image/jpeg', lastModified: Date.now() })
-                resolve({ file: resizedFile, preview: URL.createObjectURL(resizedFile) })
-              }
-              URL.revokeObjectURL(objectUrl)
-            }, 'image/jpeg', 0.8)
-          }
-
-          img.onerror = reject
-          img.src = objectUrl
-        }
       } catch (err) {
         reject(err)
       }
