162640d6d7ddbf8e5586ac0d25fef1126126016f
Marco Petutschnig <mpetutschnig@gmail.com>
2026-05-22 00:55:41 +0200

    fix: exclude /pow from SW navigation fallback so mobile doesn't get login redirect

--- pwa/vite.config.ts
+++ pwa/vite.config.ts
@@ -27,7 +27,8 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-        globIgnores: ['pow/**']
+        globIgnores: ['pow/**'],
+        navigateFallbackDenylist: [/^\/pow/]
       }
     })
   ],
